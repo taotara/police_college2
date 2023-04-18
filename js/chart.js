87421113fbdc0244d6751b7b512e542c3243efa2
taotara <taotara@yahoo.com>
2023-04-18 21:19:08 +0100

    charts implimented

---
+++ js/chart.js
@@ -0,0 +1,60 @@
+const ctx = document.getElementById('lineChart').getContext('2d');
+const myLineChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ],
+    datasets: [
+      {
+        label: 'Last week',
+        backgroundColor: 'rgba(161, 198, 247, 1)',
+        borderColor: 'rgb(47, 128, 237)',
+        data: [3000, 4000, 2000, 5000, 8000, 9000, 2000],
+      },
+    ],
+  },
+  options: {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  },
+});
+
+
+// Bar chart
+
+const ctx2 = document.getElementById('barChart');
+
+new Chart(ctx2, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
